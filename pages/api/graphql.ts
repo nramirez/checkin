948a59f183d3d2241416eef4d2345afdffd4a836
Nazaret Ramirez <nazaret@rev.com>
2020-02-09 09:18:18 -0600

    adding appollo server micro

---
+++ pages/api/graphql.ts
@@ -0,0 +1,39 @@
+import { ApolloServer, gql } from 'apollo-server-micro';
+
+const typeDefs = gql`
+    type Organization {
+        id: ID!
+        name: String
+        enabled: Boolean
+    }
+
+    input OrganizationInput {
+        name: String!
+        enabled: Boolean
+    }
+
+    type OrganizationPayload {
+        organization: Organization
+    }
+
+    type Mutation {
+        createOrganization(input: OrganizationInput!): OrganizationPayload!
+    }
+
+    type Query {
+        Organization(id: ID!): Organization
+    }
+`;
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    mockEntireSchema: false
+});
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+};
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
