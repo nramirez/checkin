c9804d032128a70adc08ec66ca12ef06ba94b82d
Nazaret Ramirez <nazaret@rev.com>
2020-02-18 08:03:55 -0600

    i don't probably need react virtualized yet

--- pages/components/organizations.tsx
+++ pages/components/organizations.tsx
@@ -1,7 +1,8 @@
-import { Paper, Table, TableContainer, TableHead, TableBody, TableRow, FormControlLabel, Checkbox, List, ListItem } from '@material-ui/core';
+import { Paper, TableContainer, TableHead, TableBody, TableRow, FormControlLabel, Checkbox, List, ListItem, Theme, createStyles } from '@material-ui/core';
 import TableCell from '@material-ui/core/TableCell';
 import useOrganizations from '../hooks/organizations.hooks';
-import InfiniteLoading from 'react-simple-infinite-loading';
+import { VirtualizedTable } from './virtualizedTable';
+import { AutoSizer, Column, Table, TableCellRenderer, TableHeaderProps } from 'react-virtualized';
 
 export const Organizations = (): JSX.Element => {
     const { organizations, loading, loadMore, hasNextPage } = useOrganizations();
@@ -10,112 +11,22 @@ export const Organizations = (): JSX.Element => {
     const isLoaded = i => !hasNextPage || i < organizations.length;
     console.log(organizations, loading);
 
-    return (
-        <Paper>
-            <div>
-                Hola {organizations.length}
-            </div>
-                
-            <InfiniteLoading
-                items={organizations}
-                itemHeight={40}
-                hasMoreItems={hasNextPage}
-                loadMoreItems={loadMoreOrganizations}>
-               
-               {({ index, style }) => {
-                    
-                    console.log(index);
-                    let content;
-                    if (!isLoaded(index)) {
-                        content = 'Loading...'
-                    } else {
-
-                        const { name, enabled } = organizations[index];
-                        content =
-                            <FormControlLabel
-                                control={
-                                    <Checkbox checked={enabled} value="checkedA" />
-                                }
-                                label={name}
-                            />
-                    }
-                    return <ListItem>
-                        {content}
-                    </ListItem>;
-                }}
-            </InfiniteLoading>
-        </Paper>
-    )
+    return <Paper style={{ height: 400, width: '100%' }}>
+        <VirtualizedTable
+            rowCount={organizations.length}
+            rowGetter={({ index }) => organizations[index]}
+            columns={[
+                {
+                    width: 200,
+                    label: 'Actions',
+                    dataKey: 'actions',
+                },
+                {
+                    width: 120,
+                    label: 'Name',
+                    dataKey: 'name'
+                }
+            ]}
+        />
+    </Paper>
 }
-
-
-/*
-
- {({ index, style }) => {
-                    
-                    console.log(index);
-                    let content;
-                    if (!isLoaded(index)) {
-                        content = 'Loading...'
-                    } else {
-
-                        const { name, enabled } = organizations[index];
-                        content =
-                            <FormControlLabel
-                                control={
-                                    <Checkbox checked={enabled} value="checkedA" />
-                                }
-                                label={name}
-                            />
-                    }
-                    return <div>
-                        {content}
-                    </div>;
-                }}
-
-
-<TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableCell>
-                        Actions
-                     </TableCell>
-                    <TableCell>
-                        Name
-                     </TableCell>
-                    <TableCell>
-                        Enabled
-                     </TableCell>
-                </TableHead>
-                <TableBody>
-                    <InfiniteLoadingList
-                        items={organizations}
-                        itemsHeight={40}
-                        hasMoreItems={hasNextPage}
-                        loadMoreItems={loadMoreOrganizations}>
-                        {({ index, style }) => {
-                            if (!isLoaded(index)) {
-                                return <TableRow>
-
-                                    <TableCell colSpan={3} >
-                                        Loading...
-                                    </TableCell>
-                                </TableRow>
-                            }
-                            const { name, enabled } = organizations[index];
-                            return <TableRow>
-                                <TableCell>
-                                    Actions
-                                   </TableCell>
-                                <TableCell>
-                                    {name}
-                                </TableCell>
-                                <TableCell>
-                                    {enabled}
-                                </TableCell>
-                            </TableRow>
-                        }}
-                    </InfiniteLoadingList>
-                </TableBody>
-            </Table>
-        </TableContainer>*/
